c8784cb458035a01a4bb2260f0aced2b06153ab0
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-11-30 12:19:47 +0100

    thats just a test commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function(req, res) {
-	res.write("On the way to being a full stack engineer!");
+	res.write("One step closer to being a full stack engineer!");
 	res.end();
 }
 ).listen(3000);
