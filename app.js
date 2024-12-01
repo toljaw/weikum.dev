42e1087aa79b7792e1ad37326581eeaf09a36166
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-12-01 12:02:57 +0100

    add index.html file

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 http.createServer(function(req, res) {
-	res.write("One step closer to being a full stack engineer!");
-	res.end();
-}
-).listen(3000);
+	const filePath = path.join(__dirname, 'index.html');
+	fs.readFile(filePath, function(err, data) {
+		if (err) {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.write('Internal Server Error');
+			res.end();
+		} else {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			res.end();
+		}
+	});
+}).listen(3000);
 
 console.log("Server started on port 3000");
