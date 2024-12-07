99d1074f1a15246e810cf8bb676164188de42fef
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-12-07 09:40:57 +0100

    test2

--- app.js
+++ app.js
@@ -3,19 +3,47 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer(function(req, res) {
-	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
 
-	fs.readFile(filePath, function(err, data) {
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.write('404 Not Found');
-			res.end();
-		} else {
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
-			res.end();
-		}
-	});
+    let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(err, data) {
+        if (err) {
+            if(err.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+err.code+' ..\n');
+                res.end();
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data, 'utf-8');
+        }
+    });
+
 }).listen(3000);
 
 console.log("Server started on port 3000");
---
+++ app_backup.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer(function(req, res) {
+	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+	fs.readFile(filePath, function(err, data) {
+		if (err) {
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.write('404 Not Found');
+			res.end();
+		} else {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			res.end();
+		}
+	});
+}).listen(3000);
+
+console.log("Server started on port 3000");
